81bfc8f34a6c315cfa5f6ef273084368a0a32b06
KarunyaGupta <mails.karunyagupta@gmail.com>
2024-08-11 15:33:29 +0530

    fe

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function displayResults (weather) {
   weather_el.innerText = weather.weather[0].main;
 
   let hilow = document.querySelector('.hi-low');
-  hilow.innerText = ` MAX ${Math.round(weather.main.temp_min)}°c / MIN ${Math.round(weather.main.temp_max)}°c`;
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
 function dateBuilder (d) {
